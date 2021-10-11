57d8aa05bc7afd732d872a954cd15a1fd849c878
Hegel <hegelty@naver.com>
2021-10-11 16:45:52 +0900

    출력을 html형식으로 + 제목

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-lastnum_file = open("lastnum","r")
-today_file = open("today","w")
-outputs_file = open("outputs","r")
-outputs = outputs_file.read()
-outputs_file.close()
-last_num = int(lastnum_file.read())
-lastnum_file.close()
+today_file = open("today", "w")
+today_string = str("")
+
 
 def find_compuzonelink(url):
-    cl_response = requests.get("https://quasarzone.com"+url)
+    cl_response = requests.get("https://quasarzone.com" + url)
     if cl_response.status_code == 200:
         clhtml = cl_response.text
         clsoup = BeautifulSoup(clhtml, 'html.parser')
@@ -21,37 +17,46 @@ def find_compuzonelink(url):
         for i in clhref:
             link = i.attrs['href']
             if str(link).startswith("https://quasarzone.com/compuzoneLink?"):
-                print(link)
-                today_file.write(link + "\n")
+                title = clsoup.select_one("#content > div.sub-content-wrap > div.view-content-wrap > div.view-content-area > div.view-title-wrap > h1").text
+                print(title + " : " + link)
+                today_file.write('<a href="'+ link + '">' + title + "</a><br>")
                 break
 
+    else:
+        print("error")
 
-qc_url = "https://quasarzone.com/bbs/qc_qsz"
-qc_response = requests.get(qc_url)
 
-if qc_response.status_code == 200:
-    html = qc_response.text
-    soup = BeautifulSoup(html, 'html.parser')
+def main():
+    lastnum_file = open("lastnum", "r")
+    today_file = open("today", "w")
+    last_num = int(lastnum_file.read())
+    lastnum_file.close()
 
-    href = soup.find("div",{'class':'list-wrap'})
-    href = href.find_all("a")
+    qc_url = "https://quasarzone.com/bbs/qc_qsz"
+    qc_response = requests.get(qc_url)
 
-    lastnum_changed = False
+    if qc_response.status_code == 200:
+        html = qc_response.text
+        soup = BeautifulSoup(html, 'html.parser')
 
-    for i in href:
-        link = i.attrs['href']
-        if int(str(link)[18:]) > last_num:
-            if not lastnum_changed:
-                lastnum_file = open("lastnum", "w")
-                lastnum_file.write(str(link)[18:])
-                lastnum_changed = True
+        href = soup.find("div", {'class': 'list-wrap'})
+        href = href.find_all("a")
 
-            print(int(str(link)[18:]))
-            find_compuzonelink(str(link))
+        lastnum_changed = False
 
-    today_file.close()
-    today_file = open("today","r")
-    outputs_file = open("outputs","w")
-    outputs_file.write(today_file.read() + outputs)
-else :
-    print(qc_response.status_code)
+        for i in href:
+            link = i.attrs['href']
+            if int(str(link)[18:]) > last_num:
+                if not lastnum_changed:
+                    lastnum_file = open("lastnum", "w")
+                    lastnum_file.write(str(link)[18:])
+                    lastnum_changed = True
+
+                print(int(str(link)[18:]))
+                find_compuzonelink(str(link))
+
+        today_file.close()
+    else:
+        print(qc_response.status_code)
+
+main()
