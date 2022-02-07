5caa2cbc348b084561b39f76a950fda3448d4564
Hegel <hegelty@naver.com>
2022-02-07 14:49:46 +0900

    퀘이사존 업로드용으로 나오도록 만듦

--- main.py
+++ main.py
@@ -1,62 +1,46 @@
 import requests
 from bs4 import BeautifulSoup
 
-today_file = open("today", "w")
-today_string = str("")
+output_file = open("output.txt", "w")
 
 
-def find_compuzonelink(url):
-    cl_response = requests.get("https://quasarzone.com" + url)
+def find_compuzonelink(id):
+    url = "https://quasarzone.com/bbs/qc_qsz/views/" + str(id)
+    cl_response = requests.get(url)
     if cl_response.status_code == 200:
         clhtml = cl_response.text
         clsoup = BeautifulSoup(clhtml, 'html.parser')
 
-        clhref = clsoup.find("div", {'class': 'view-content'})
-        clhref = clhref.find_all("a")
+        clhref = clsoup.find(class_="compuzone-editor-wrap-wrap")
+        print(clhref)
 
-        for i in clhref:
-            link = i.attrs['href']
-            if str(link).startswith("https://quasarzone.com/compuzoneLink?"):
-                title = clsoup.select_one("#content > div.sub-content-wrap > div.view-content-wrap > div.view-content-area > div.view-title-wrap > h1").text
-                print(title + " : " + link)
-                today_file.write('<a href="'+ link + '">' + title + "</a><br>")
-                break
+        if(clhref):
+            title = clsoup.find(class_="title pr-0").text.strip()
+            print(title)
+            output_file.write(f'<a href="{url}">칼럼 보러가기 : {title}<br></a>{str(clhref)}<br>')
 
     else:
         print("error")
 
 
 def qsz_parse():
-    lastnum_file = open("lastnum", "r")
-    today_file = open("today", "w")
-    last_num = int(lastnum_file.read())
-    lastnum_file.close()
+    qc_url = "https://quasarzone.com/bbs/qc_qsz?page="
+    for i in range(1,30):
+        qc_response = requests.get(qc_url+str(i))
 
-    qc_url = "https://quasarzone.com/bbs/qc_qsz"
-    qc_response = requests.get(qc_url)
+        if qc_response.status_code == 200:
+            html = qc_response.text
+            soup = BeautifulSoup(html, 'html.parser')
 
-    if qc_response.status_code == 200:
-        html = qc_response.text
-        soup = BeautifulSoup(html, 'html.parser')
+            href = soup.find("div", {'class': 'list-wrap'})
+            href = href.find_all("a")
 
-        href = soup.find("div", {'class': 'list-wrap'})
-        href = href.find_all("a")
-
-        lastnum_changed = False
-
-        for i in href:
-            link = i.attrs['href']
-            if int(str(link)[18:]) > last_num:
-                if not lastnum_changed:
-                    lastnum_file = open("lastnum", "w")
-                    lastnum_file.write(str(link)[18:])
-                    lastnum_changed = True
-
-                print(int(str(link)[18:]))
-                find_compuzonelink(str(link))
-
-        today_file.close()
-    else:
-        print(qc_response.status_code)
+            for i in href:
+                link = i.attrs['href']
+                id = int((str(link)[18:]).split('?')[0])
+                print(id)
+                find_compuzonelink(id)
+        else:
+            print(qc_response.status_code)
 
 qsz_parse()
